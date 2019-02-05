7a78ceb694ac2085c5fe03a631ccb6cec87ecfe4
anhptvt1992 <lambogini1992@gmail.com>
2019-02-05 14:20:07 +0700

    This cource code used for rotation matrix

--- rotation_matrix.cpp
+++ rotation_matrix.cpp
@@ -6,40 +6,40 @@
 using namespace std;
 /*variable r is time of rotation matrix*/
 
-void rotation_array(vector<int> *array, int r)
-{
-  vector<int> array_temp (array->size(), 1);
-  int length_array;
-  int data, pos;
-
-  data = 0;
-  length_array = array_temp.size();
-
-  for (int i = 0; i < length_array; i++)
-  {
-    data = array->at(i);
-    if( length_array > i + r)
-    {
-      pos = i + r;
-    }
-    else
-    {
-      pos = i + r - length_array;
-    }
-    array_temp[pos] = data;
-  }
-  array_temp.swap(*array);
-}
-
-void return_matrix(vector<vector<int>> *matrix, vector<int> array, int pos, int length_row, int length_collum)
-{
-  int i, j, k, z;
-  k = z = pos;
-  for(i = z; i <= length_collum, i++)
-  {
-
-  }
-}
+// void rotation_array(vector<int> *array, int r)
+// {
+//   vector<int> array_temp (array->size(), 1);
+//   int length_array;
+//   int data, pos;
+//
+//   data = 0;
+//   length_array = array_temp.size();
+//
+//   for (int i = 0; i < length_array; i++)
+//   {
+//     data = array->at(i);
+//     if( length_array > i + r)
+//     {
+//       pos = i + r;
+//     }
+//     else
+//     {
+//       pos = i + r - length_array;
+//     }
+//     array_temp[pos] = data;
+//   }
+//   array_temp.swap(*array);
+// }
+//
+// void return_matrix(vector<vector<int>> *matrix, vector<int> array, int pos, int length_row, int length_collum)
+// {
+//   int i, j, k, z;
+//   k = z = pos;
+//   for(i = z; i <= length_collum, i++)
+//   {
+//
+//   }
+// }
 
 void rotation_matrix(vector<vector<int>> matrix, int r)
 {
@@ -57,10 +57,10 @@ void rotation_matrix(vector<vector<int>> matrix, int r)
 
   matrix_temp.resize(length_row);
   matrix_temp[0].resize(length_collum);
-
+  
   while((row_count < length_row) && (collum_count < length_collum))
   {
-    for
+    for()
   }
 
 }
