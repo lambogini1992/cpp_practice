23dbcecddcaccfd5c34d8030080834a17f1575bf
OEM Configuration (temporary user) <phamtuananhvnvt1992@gmail.com>
2020-04-06 23:11:38 +0700

    abcd

---
+++ bootloader_app_pc/Debug/src/main.d
@@ -0,0 +1,13 @@
+src/main.o: ../src/main.c \
+ /home/oem/git_clone/cpp_practice/bootloader_app_pc/COMMON_INCLUDE/common_include.h \
+ /home/oem/git_clone/cpp_practice/bootloader_app_pc/FILE_HANDLE/file_handle.h \
+ /home/oem/git_clone/cpp_practice/bootloader_app_pc/BOOT_PROC/boot_proc.h \
+ /home/oem/git_clone/cpp_practice/bootloader_app_pc/SERIAL_PORT/serial_port.h
+
+/home/oem/git_clone/cpp_practice/bootloader_app_pc/COMMON_INCLUDE/common_include.h:
+
+/home/oem/git_clone/cpp_practice/bootloader_app_pc/FILE_HANDLE/file_handle.h:
+
+/home/oem/git_clone/cpp_practice/bootloader_app_pc/BOOT_PROC/boot_proc.h:
+
+/home/oem/git_clone/cpp_practice/bootloader_app_pc/SERIAL_PORT/serial_port.h:
